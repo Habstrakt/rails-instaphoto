9b218fa3b7484a30620b0c4526fdaf1f94158a11
Habstrakt <habstrkt@ya.ru>
2019-05-20 11:50:07 +0800

    ассоциация user и photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :owner, only: [:edit, :update, :destroy]
 
   
   def index
@@ -12,7 +14,7 @@ class PhotosController < ApplicationController
 
   
   def new
-    @photo = Photo.new
+    @photo = current_user.photos.build
   end
 
   
@@ -21,7 +23,7 @@ class PhotosController < ApplicationController
 
   
   def create
-    @photo = Photo.new(photo_params)
+    @photo = current_user.photos.build
       if @photo.save
         redirect_to @photo, notice: 'Photo was successfully created.'
       else
@@ -45,6 +47,11 @@ class PhotosController < ApplicationController
   end
 
   private
+
+    def owner
+      @photo = current_user.photos.find_by(id: params[:id])
+      redirect_to photos_path, notice: "У нас нет разрешения на изменение этой фотографии" if @photo.nil?
+    end
     
     def set_photo
       @photo = Photo.find(params[:id])
