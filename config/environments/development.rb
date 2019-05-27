9894427235570e730a5e7b0bda7c102d9ef6dcbc
Habstrakt <habstrkt@ya.ru>
2019-05-27 23:15:36 +0800

    devise confibnable and email confirrm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'localhost:3000',
+  user_name:            '<username>',
+  password:             '<password>',
+  authentication:       'plain',
+  enable_starttls_auto: true }
 end
